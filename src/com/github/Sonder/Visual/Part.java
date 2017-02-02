bb85e30337374891bf3e4127be5cf90779a3b31c
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-02-01 20:38:56 -0800

    Allow parts to issue commands to the ship

--- src/com/github/Sonder/Visual/Part.java
+++ src/com/github/Sonder/Visual/Part.java
@@ -5,22 +5,36 @@ import java.awt.geom.Point2D;
 import java.util.HashMap;
 
 public abstract class Part {
+    private final Ship2 parent;
+
     private final HashMap<String, Double> attributes;
 
     private final Drawn shape;
+
+    public final HashMap<String, Ship2.Command> actions;
 
     /**
      * Holds the x and y coordinates of the shape relative to the rest of the ship.
      */
     private final Point2D.Double location;
 
-    public Part(Drawn shape, HashMap<String, Double> attributes, Point2D.Double location) {
+    public Part(Ship2 parent, Drawn shape, HashMap<String, Double> attributes, Point2D.Double location, HashMap<String, Ship2.Command> actions) {
+        this.parent = parent;
         this.shape = shape;
         this.attributes = attributes;
         this.location = location;
+        this.actions = actions;
     }
 
     public abstract void update();
+
+    public Ship2.Command action(String key) {
+        return actions.get(key);
+    }
+
+    public final String[] getKeys() {
+        return (String[]) actions.keySet().toArray();
+    }
 
     public final Drawn getShape() {
         return shape;
--- src/com/github/Sonder/Visual/Ship2.java
+++ src/com/github/Sonder/Visual/Ship2.java
@@ -1,28 +1,53 @@
 package com.github.Sonder.Visual;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Ship2 {
-    private HashMap<String, Double> attributes;
+    private HashMap<String, ArrayList<Command>> triggers;
 
     private Part[] parts;
 
+    public final double[] vector;
+
+    public void move() {
+        for (Part part : parts) {
+            part.getShape().translate(vector[0], vector[1]);
+        }
+    }
+
     public void update() {
+        move();
+
         for (Part part : parts) {
             part.update();
         }
     }
 
-    private String[] keys =
-            {
-                    "rotate counter clockwise key",
-                    "rotate clockwise key",
-                    "thrust key",
-                    "fire key"
-            };
+    public void trigger(String key) {
+        for (Command command : triggers.get(key)) {
+            command.apply();
+        }
+    }
 
-    public Ship2(Part[] parts, String[] keys) {
+    public Ship2(Part[] parts) {
         this.parts = parts;
-        this.keys = keys;
+
+        vector = new double[2];
+
+        triggers.put("thrust", new ArrayList<>());
+        triggers.put("rotate counter clockwise", new ArrayList<>());
+        triggers.put("rotate clockwise", new ArrayList<>());
+        triggers.put("turrets", new ArrayList<>());
+
+        for (Part part : parts) {
+            for (String key : part.getKeys()) {
+                triggers.get(key).add(part.action(key));
+            }
+        }
+    }
+
+    interface Command {
+        void apply();
     }
 }
