dae10526fb55461edd9a8977582843faa36c109b
Michael MacLeod <michaelmmacleod@gmail.com>
2017-02-03 14:59:54 -0800

    Make server start and look for connections.

--- src/com/github/Sonder/server/Sonder.java
+++ src/com/github/Sonder/server/Sonder.java
@@ -1,5 +1,34 @@
 package com.github.Sonder.server;
 
-public class Sonder {
-    void update() {}
+import java.net.Socket;
+
+public class Sonder extends Thread {
+
+    private static final int MS_PER_UPDATE = 10;
+
+    public void run() {
+        double previous = System.currentTimeMillis();
+        double lag = 0;
+
+        // TODO: add a key to exit the application
+        while (true) {
+            double current = System.currentTimeMillis();
+            double elapsed = current - previous;
+            previous = current;
+            lag += elapsed;
+
+            while (lag >= MS_PER_UPDATE) {
+                update();
+                lag -= MS_PER_UPDATE;
+            }
+        }
+    }
+
+    public void connectPlayer(Socket player) {
+
+    }
+
+    private void update() {
+
+    }
 }
--- src/com/github/Sonder/server/SonderServer.java
+++ src/com/github/Sonder/server/SonderServer.java
@@ -1,26 +1,44 @@
 package com.github.Sonder.server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class SonderServer {
 
-    private static final int MS_PER_UPDATE = 10;
+    private static final int DEFAULT_PORT = 5782;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        log("Starting SonderServer...");
+
         Sonder game = new Sonder();
+        game.start();
 
-        double previous = System.currentTimeMillis();
-        double lag = 0;
+        ServerSocket listener = new ServerSocket(DEFAULT_PORT);
 
-        // TODO: add a key to exit the application
-        while (true) {
-            double current = System.currentTimeMillis();
-            double elapsed = current - previous;
-            previous = current;
-            lag += elapsed;
-
-            while (lag >= MS_PER_UPDATE) {
-                game.update();
-                lag -= MS_PER_UPDATE;
+        if (args.length > 0) {
+            try {
+                int port = Integer.parseInt(args[0]);
+                listener = new ServerSocket(port);
+                log("Started server on custom port: " + port);
+            } catch (Exception e) {
+                log("Error: Port '" + args[0] + "' is not a number");
+                log("Stopping Server...");
+                System.exit(1);
             }
+        } else {
+            log("Started server on default port: " + DEFAULT_PORT);
         }
+
+        try {
+            while (true) {
+                game.connectPlayer(listener.accept());
+            }
+        } catch(IOException e) {
+
+        }
+    }
+
+    private static void log(String text) {
+        System.out.println("SonderServer: " + text);
     }
 }
