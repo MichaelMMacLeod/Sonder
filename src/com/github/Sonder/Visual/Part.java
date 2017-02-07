d6f4c910a7d2b4fc1a91db5d91b3a0dcf11854a2
Michael MacLeod <michaelmmacleod@gmail.com>
2017-02-07 14:54:11 -0800

    Create basic classes and methods

---
+++ src/com/github/Sonder/Visual/Part.java
@@ -0,0 +1,9 @@
+package com.github.Sonder.Visual;
+
+public class Part {
+    private Drawn shape;
+
+    public Part(Drawn shape) {
+        this.shape = shape;
+    }
+}
---
+++ src/com/github/Sonder/Visual/PartCollection.java
@@ -0,0 +1,14 @@
+package com.github.Sonder.Visual;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
+public class PartCollection {
+    private Point2D.Double location;
+    private double rotation;
+    private ArrayList<Part> parts;
+
+//    public double[][][] getVertices() {
+//
+//    }
+}
