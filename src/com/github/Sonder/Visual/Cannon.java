e99bd00d41e3eb743c78dd2a587af41e616cb84b
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-03-09 16:35:26 -0800

    Make the Cannon shape look better

--- src/com/github/Sonder/Visual/Cannon.java
+++ src/com/github/Sonder/Visual/Cannon.java
@@ -5,9 +5,9 @@ import java.awt.*;
 public class Cannon extends Poly {
     public Cannon(double x, double y) {
         super(
-                new double[] {-30, 30, 30, 35, 35, 30, 30, -30},
-                new double[] { -5, -5,-10,-10, 10, 10, 5, 5},
-                8,
+                new double[] {-30,  0, 30,  30,  50, 50, 30, 30, 0, -30},
+                new double[] {-20, -5, -5, -10, -10, 10, 10,  5, 5,  20},
+                10,
                 0,
                 0,
                 x,
@@ -18,7 +18,7 @@ public class Cannon extends Poly {
                 new double[] {},
                 new double[] {},
                 0,
-                -20,
+                -30,
                 0);
     }
 }
