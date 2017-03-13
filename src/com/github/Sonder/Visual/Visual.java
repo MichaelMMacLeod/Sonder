714e2e4f07cfa140fd91757f8659056a016a7a17
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-03-12 21:35:15 -0700

    Add the Visual class

--- src/com/github/Sonder/Visual/Visual.java
+++ src/com/github/Sonder/Visual/Visual.java
@@ -8,6 +8,9 @@ public abstract class Visual {
     private Point2D.Double center;
     private Line2D.Double[] lines;
 
+    /**
+     * @return a copy of the lines in this object.
+     */
     public final Line2D.Double[] getLines() {
         Line2D.Double[] copy = new Line2D.Double[lines.length];
         for (int i = 0; i < copy.length; i++) {
@@ -16,24 +19,49 @@ public abstract class Visual {
         return copy;
     }
 
-    public Visual(Point2D.Double center, Line2D.Double[] lines) {
-        this.center = new Point2D.Double(center.x, center.y);
-
+    /**
+     * Creates a visual object defined by a series of lines which can be translated and rotated.
+     * @param lines  is the list of lines to be copied into this object.
+     * @param center is the center of rotation to be copied into this object.
+     */
+    public Visual(Line2D.Double[] lines, Point2D.Double center) {
         this.lines = new Line2D.Double[lines.length];
         for (int i = 0; i < this.lines.length; i++)
             this.lines[i] = new Line2D.Double(lines[i].x1, lines[i].y1, lines[i].x2, lines[i].y2);
+
+        this.center = new Point2D.Double(center.x, center.y);
     }
 
-    public void update() {
+    /**
+     * Sets the center of rotation to an (x, y) coordinate.
+     * @param x is the x coordinate.
+     * @param y is the y coordinate.
+     */
+    public final void setCenter(double x, double y) {
+        center.setLocation(x, y);
+    }
+
+    /**
+     * Applies the current transformation.
+     */
+    public final void transform() {
         translate();
         rotate();
     }
 
+    /**
+     * Appends a translation to the current transformation.
+     * @param dx is the distance in the x dimension.
+     * @param dy is the distance in the y dimension.
+     */
     public final void translate(double dx, double dy) {
         this.dx += dx;
         this.dy += dy;
     }
 
+    /**
+     * Applies the current translation and then resets it to zero.
+     */
     private void translate() {
         for (Line2D.Double line : lines) {
             line.setLine(
@@ -41,14 +69,23 @@ public abstract class Visual {
                     line.x2 + dx, line.y2 + dy);
         }
 
+        center.setLocation(center.x + dx, center.y + dy);
+
         dx = 0;
         dy = 0;
     }
 
+    /**
+     * Appends a rotation to the current transformation.
+     * @param dt is the angle in radians.
+     */
     public final void rotate(double dt) {
         this.dt += dt;
     }
 
+    /**
+     * Applies the current rotation and then resets it to zero.
+     */
     private void rotate() {
         double cos = Math.cos(dt), sin = Math.sin(dt);
 
