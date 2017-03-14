a08b1ea203072a5c8551edd1c0cd5fda1d2ee391
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-03-13 18:46:25 -0700

    Use Point2D.Double in Visual instead of Line2D.Double

--- src/com/github/Sonder/Visual/Visual.java
+++ src/com/github/Sonder/Visual/Visual.java
@@ -1,35 +1,34 @@
 package com.github.Sonder.Visual;
 
-import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 
 public abstract class Visual {
-    private double dx, dy, dt;
-    private Point2D.Double center;
-    private Line2D.Double[] lines;
-
     /**
-     * @return a copy of the lines in this object.
-     */
-    public final Line2D.Double[] getLines() {
-        Line2D.Double[] copy = new Line2D.Double[lines.length];
-        for (int i = 0; i < copy.length; i++) {
-            copy[i] = new Line2D.Double(lines[i].x1, lines[i].y1, lines[i].x2, lines[i].y2);
-        }
-        return copy;
-    }
-
-    /**
-     * Creates a visual object defined by a series of lines which can be translated and rotated.
-     * @param lines  is the list of lines to be copied into this object.
+     * Creates a visual object defined by a series of points which can be translated and rotated.
+     * @param points is the list of points to be copied into this object.
      * @param center is the center of rotation to be copied into this object.
      */
-    public Visual(Line2D.Double[] lines, Point2D.Double center) {
-        this.lines = new Line2D.Double[lines.length];
-        for (int i = 0; i < this.lines.length; i++)
-            this.lines[i] = new Line2D.Double(lines[i].x1, lines[i].y1, lines[i].x2, lines[i].y2);
+    public Visual(Point2D.Double[] points, Point2D.Double center) {
+        this.points = new Point2D.Double[points.length];
+        for (int i = 0; i < this.points.length; i++)
+            this.points[i] = new Point2D.Double(points[i].x, points[i].y);
 
         this.center = new Point2D.Double(center.x, center.y);
+    }
+
+    private double dx, dy, dt;
+    private Point2D.Double center;
+    private Point2D.Double[] points;
+
+    /**
+     * @return a copy of the points in this object.
+     */
+    public final Point2D.Double[] getPoints() {
+        Point2D.Double[] copy = new Point2D.Double[points.length];
+        for (int i = 0; i < copy.length; i++) {
+            copy[i] = new Point2D.Double(points[i].x, points[i].y);
+        }
+        return copy;
     }
 
     /**
@@ -60,22 +59,6 @@ public abstract class Visual {
     }
 
     /**
-     * Applies the current translation and then resets it to zero.
-     */
-    private void translate() {
-        for (Line2D.Double line : lines) {
-            line.setLine(
-                    line.x1 + dx, line.y1 + dy,
-                    line.x2 + dx, line.y2 + dy);
-        }
-
-        center.setLocation(center.x + dx, center.y + dy);
-
-        dx = 0;
-        dy = 0;
-    }
-
-    /**
      * Appends a rotation to the current transformation.
      * @param dt is the angle in radians.
      */
@@ -84,18 +67,27 @@ public abstract class Visual {
     }
 
     /**
+     * Applies the current translation and then resets it to zero.
+     */
+    private void translate() {
+        for (Point2D.Double point : points)
+            point.setLocation(point.x + dx, point.y + dy);
+
+        center.setLocation(center.x + dx, center.y + dy);
+
+        dx = 0;
+        dy = 0;
+    }
+
+    /**
      * Applies the current rotation and then resets it to zero.
      */
     private void rotate() {
         double cos = Math.cos(dt), sin = Math.sin(dt);
 
-        for (Line2D.Double line : lines) {
-            Line2D.Double prime = new Line2D.Double(
-                    line.x1 - center.x, line.y1 - center.y,
-                    line.x2 - center.x, line.y2 - center.y);
-            line.setLine(
-                    prime.x1 * cos - prime.y1 * sin + center.x, prime.x1 * sin + prime.y1 * cos + center.y,
-                    prime.x2 * cos - prime.y2 * sin + center.x, prime.x2 * sin + prime.y2 * cos + center.y);
+        for (Point2D.Double point : points) {
+            Point2D.Double prime = new Point2D.Double(point.x - center.x, point.y - center.y);
+            point.setLocation(prime.x * cos - prime.y * sin, prime.x * sin + prime.y * cos);
         }
 
         dt = 0;
